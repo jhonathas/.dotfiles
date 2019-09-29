198fb7669bde3f5475a04c8f42e070e3f58d011e
Diego Nogueira <diegoalvareznogueira@gmail.com>
2019-09-29 12:12:21 -0300

    changes

--- zsh/.zsh.d/scripts/options.sh
+++ zsh/.zsh.d/scripts/options.sh
@@ -20,26 +20,11 @@ export STOW_DIR=$HOME/.dotfiles
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
   source /usr/share/doc/fzf/examples/key-bindings.zsh
+
+  alert() {
+    notify-send --icon=gtk-info Alert $1
+  }
 fi
 
 source ~/.config/tmuxinator/tmuxinator.zsh
 
-deploy () {
-  environment=$1
-
-  branch=$(git branch | grep \* | cut -d ' ' -f2)
-  [ -n "${2}" ] && branch=$2
-
-  rm -rf .deliver/releases
-  mix edeliver build release to $environment --branch=$branch
-  mix edeliver deploy release to $environment
-
-  mix edeliver stop -- $environment
-}
-
-run_on () {
-  ip=$1
-  command=$2
-
-  ssh ubuntu@$ip "source ~/.profile && $command"
-}
