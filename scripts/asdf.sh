8d1201ce16f8bf7b209422298ea28b3460114cdc
Diego Nogueira <diegoalvareznogueira@gmail.com>
2019-10-09 14:47:18 -0300

    added dmenu scripts

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -14,17 +14,17 @@ if ! [[ -d ~/.asdf ]]; then
   asdf plugin-add postgres https://github.com/smashedtoatoms/asdf-postgres.git
   asdf plugin-add redis https://github.com/smashedtoatoms/asdf-redis.git
   asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-  # asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-  # bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+  asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 
   asdf install erlang 22.0.1
   asdf install elixir 1.9.1
   asdf install redis 4.0.10
   asdf install postgres 10.4
   asdf install ruby 2.6.4
-  #asdf install nodejs 10.16.3
+  asdf install nodejs 10.16.3
 
-  #asdf global nodejs 10.16.3
+  asdf global nodejs 10.16.3
   asdf global erlang 22.0.1
   asdf global elixir 1.9.1
   asdf global redis 4.0.10
