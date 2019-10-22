40ee033f4eb0b9050b17b02f2b4d2fba5cbeae5d
Jhonathas Matos <jhonathas@gmail.com>
2019-10-22 15:54:27 -0300

    improvements

--- scripts/others.sh
+++ scripts/others.sh
@@ -8,4 +8,8 @@ echo ""
 
 if ! [[ -d ~/.jabba ]]; then
   curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+
+  jabba install openjdk@1.11.0-2
+  jabba use openjdk@1.11.0-2
+  jabba alias default openjdk@1.11.0-2
 fi
